fab4782dc48204523ccfbe7240c752698cab98c3
timlubanga <timlubanga@gmail.com>
2021-04-22 05:44:29 +0300

    configure frontend

--- meetupfrontend/src/axios.js
+++ meetupfrontend/src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const proxy = 'https://cors-anywhere.herokuapp.com/';
 const instance = axios.create({
-  baseURL: `http://localhost:3300/api/v1`,
+  baseURL: `https://meetup-tim.herokuapp.com/api/v1`,
 
   headers: { 'X-Custom-Header': '' },
 });
